cf66655d2368cc30ecadf8cf7dc5ecb5aad5bf62
x3imal <aasurov@gmail.com>
2023-03-09 14:04:20 +0300

    1 task completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        task1();
+
+
+    }
+
+    public static void task1(){
+        System.out.println("Задача 1");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год ");
+        enterYears(scanner.nextInt());
+    }
+    public static void enterYears (int year){
+        //метод вычисления вида года к Задаче 1
+        if ((year % 4 == 0) && ((year % 400 == 0) || (year % 100 != 0))) {
+            System.out.println(+year+ " високосный год");
+        } else {
+            System.out.println(+year+ " не високосный год");
+        }
+    }
+
+    public static void task2(){
+
+
     }
 }
