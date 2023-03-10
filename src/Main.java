c58c10d690cde748ae454479930dc9203f4da572
x3imal <aasurov@gmail.com>
2023-03-10 18:12:45 +0300

    3.2 task completed

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,39 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
-        task1();
-        task2();
+    public static void main(String[] args) {
+        //task1();
+        //task2();
         task3();
+
     }
 
 
-
-    public static void task1(){
+    public static void task1() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите год: ");
-        enterYears(scanner.nextInt());
+        printIsLeapYear(scanner.nextInt());
     }
-    public static void enterYears (int year){
+
+    public static void printIsLeapYear(int year) {
         //метод вычисления вида года к Задаче 1
         if ((year % 4 == 0) && ((year % 400 == 0) || (year % 100 != 0))) {
-            System.out.println(+year+ " високосный год");
+            System.out.println(+year + " високосный год");
         } else {
-            System.out.println(+year+ " не високосный год");
+            System.out.println(+year + " не високосный год");
         }
         System.out.println();
     }
 
-    public static void task2(){
+    public static void task2() {
         System.out.println("Задача 2");
         int ios = 1;
         int currentYear = LocalDate.now().getYear();
-        enterOsOptionAndYears(ios, currentYear);
+        versionСalculat(ios, currentYear);
 
     }
-    public static void enterOsOptionAndYears(int clientOs, int clientDeviceYear) {
+
+    public static void versionСalculat(int clientOs, int clientDeviceYear) {
         //метод вычисления версии OS
         if (clientOs == 0 && clientDeviceYear >= 2015) {
             System.out.println("Скачайте полную версию с AppleStore");
@@ -48,26 +50,28 @@ public class Main {
     }
 
 
-    public static void task3(){
+    public static void task3() {
         System.out.println("Задача 3");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите дистанцию до Вас: ");
-        enterDistance(scanner.nextInt());
+        //calculateDistance(scanner.nextInt());
+        int deliveryDays = calculateDistance(scanner.nextInt());
+
+
     }
 
-    public static int enterDistance(int deliveryDistance){
+    public static int calculateDistance(int deliveryDistance) {
 
-        int deliveryTime = 1;
+        int deliveryDays = 1;
         if (deliveryDistance <= 20) {
-            System.out.println("Потребуется дней: " + deliveryTime);
+            System.out.println("Потребуется дней: " + deliveryDays);
         } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            System.out.println("Потребуется дней: " + (deliveryTime + 1));
+            System.out.println("Потребуется дней: " + (deliveryDays + 1));
         } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            System.out.println("Потребуется дней: " + (deliveryTime + 2));
+            System.out.println("Потребуется дней: " + (deliveryDays + 2));
         } else {
             System.out.println("Доставки нет");
         }
-        return deliveryTime;
-
+        return deliveryDays;
     }
 }
